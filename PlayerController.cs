eb1d8416129b3c828849136bc3b1529f1cd2b4fe
remirya-kisyousyu <155557223+remirya-kisyousyu@users.noreply.github.com>
2024-03-14 01:01:23 +0900

    Add files via upload

---
+++ PlayerController.cs
@@ -0,0 +1,107 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public bool jumpFlag = false; //ジャンプ中フラグ
+    public bool gamePlayingFlag = true; //ゲーム中フラグ
+
+    private float playerSpeed = 6.0f; //移動速度
+    private float playerJump = 30.0f; //ジャンプの高さ
+    private string playerDir = "right"; //右向き
+
+    private Rigidbody2D rb; //プレイヤーのrigidbody
+    private SpriteRenderer sr; //プレイヤーのspriterenderer
+    private Transform tr; //プレイヤーのtransform
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        tr = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void FixedUpdate()
+    {
+        //ゲーム中
+        if(gamePlayingFlag == true)
+        {
+            //左右のキー入力を取得
+            float keyX = Input.GetAxis("Horizontal");
+
+            //キー入力に応じてプレイヤーを移動
+            rb.velocity = new Vector2(keyX * playerSpeed, rb.velocity.y);
+
+            //プレイヤーの向きを反転
+            FlipPlayer(keyX);
+
+            //スペースのキー入力を取得
+            bool keySpace = Input.GetKey(KeyCode.Space);
+
+            if (keySpace == true && jumpFlag == false)
+            {
+                jumpFlag = true; //二段ジャンプを受け付けない
+
+                //ジャンプ処理
+                rb.AddForce(tr.up * playerJump, ForceMode2D.Impulse);
+            }
+        }
+    }
+
+    //プレイヤーの向きを反転
+    private void FlipPlayer(float keyX)
+    {
+        if (keyX > 0 && playerDir != "right")
+        {
+            sr.flipX = false;
+            playerDir = "right";
+        }
+        else if (keyX < 0 && playerDir != "left")
+        {
+            sr.flipX = true;
+            playerDir = "left";
+        }
+        else return;
+    }
+
+    //プレイヤーと地面が接触しているか
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            //ジャンプを可能にする
+            jumpFlag = false;
+        }
+
+        else if (collision.gameObject.CompareTag("MoveBlock"))
+        {
+            jumpFlag = false;
+        }
+    }
+
+    //トリガーと接触したか
+    private void OnTriggerEnter2D(Collider2D collider)
+    {
+        if (collider.gameObject.CompareTag("Goal"))
+        {
+            GameClear();
+        }
+    }
+
+    //ゲームクリア
+    private void GameClear()
+    {
+        //キー入力の停止
+        gamePlayingFlag = false;
+        Debug.Log("game clear");
+    }
+
+}
